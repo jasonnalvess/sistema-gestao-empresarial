b09b53dd1219821d8fee237287649bcec9e5bc4c
jasonnalvess <jasonnalvess@gmail.com>
2026-07-26 16:25:53 -0300

    refactor(usuarios): padroniza isolamento multiempresa

--- backend/src/usuarios/usuarios.service.spec.ts
+++ backend/src/usuarios/usuarios.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
 import { UsuariosService } from './usuarios.service';
 
 describe('UsuariosService', () => {
   let service: UsuariosService;
 
+  const prismaServiceMock = {
+    usuario: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      count: jest.fn(),
+      update: jest.fn(),
+    },
+    $transaction: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsuariosService],
+      providers: [
+        UsuariosService,
+        {
+          provide: PrismaService,
+          useValue: prismaServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<UsuariosService>(UsuariosService);
   });
 
-  it('should be defined', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('deve estar definido', () => {
     expect(service).toBeDefined();
   });
 });
