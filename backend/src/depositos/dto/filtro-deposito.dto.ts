bf9af808f3ba74377992afba7a1dbd290c0039a6
jasonnalvess <jasonnalvess@gmail.com>
2026-07-26 16:20:30 -0300

    refactor(depositos): padroniza usuário autenticado

--- backend/src/depositos/dto/filtro-deposito.dto.ts
+++ backend/src/depositos/dto/filtro-deposito.dto.ts
@@ -1,8 +1,5 @@
 import { Transform } from 'class-transformer';
-import {
-  IsBoolean,
-  IsOptional,
-} from 'class-validator';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 import { QueryBaseDto } from '../../common/dto/query-base.dto';
 
