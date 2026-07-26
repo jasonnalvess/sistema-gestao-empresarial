67cf6649e00a2c15f2c56aee4b30577e5944d166
jasonnalvess <jasonnalvess@gmail.com>
2026-07-26 16:18:40 -0300

    refactor(fornecedores): padroniza usuário autenticado

--- backend/src/fornecedores/dto/filtro-fornecedores.dto.ts
+++ backend/src/fornecedores/dto/filtro-fornecedores.dto.ts
@@ -1,10 +1,5 @@
 import { Transform } from 'class-transformer';
-import {
-  IsBoolean,
-  IsOptional,
-  IsString,
-  Length,
-} from 'class-validator';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 
 import { QueryBaseDto } from '../../common/dto/query-base.dto';
 
--- backend/src/fornecedores/fornecedores.service.spec.ts
+++ backend/src/fornecedores/fornecedores.service.spec.ts
@@ -1,15 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
 import { FornecedoresService } from './fornecedores.service';
 
 describe('FornecedoresService', () => {
   let service: FornecedoresService;
 
+  const prismaMock = {
+    fornecedor: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      count: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+    },
+    fornecedorHistorico: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+    },
+    $transaction: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FornecedoresService],
+      providers: [
+        FornecedoresService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     service = module.get<FornecedoresService>(FornecedoresService);
+
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
